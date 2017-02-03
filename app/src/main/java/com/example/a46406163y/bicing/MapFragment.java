fcc8b3c7613c372638a36762ab7b2f5e60523610
David <46406163y@iespoblenou.org>
2017-02-03 16:22:33 +0100

    Creacio del MapFragment i el seu layout

---
+++ app/src/main/java/com/example/a46406163y/bicing/MapFragment.java
@@ -0,0 +1,24 @@
+package com.example.a46406163y.bicing;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewGroupCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by 46406163y on 03/02/17.
+ */
+
+public class MapFragment extends Fragment {
+
+    public MapFragment(){
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+}
